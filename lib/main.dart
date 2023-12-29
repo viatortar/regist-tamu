525d89039c410cf0f9eafdddb3887c4729910d19
raketyonex <154741306+raketyonex@users.noreply.github.com>
2023-12-29 20:48:44 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Lowongan Pekerjaan',
+      title: 'Lowongan KERJA',
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
     );
